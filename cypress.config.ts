1b895f3b372643acab16a1fc57ee5d74de4456f7
lexiewh <lexwhite028@gmail.com>
2023-05-07 21:43:19 -0500

    fix cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
 	fileServerFolder: 'dist',
 	fixturesFolder: false,
-	projectId: 'etow1b',
+	projectId: '3dkdwt',
 	e2e: {
 		baseUrl: 'http://localhost:4173/',
 		specPattern: 'cypress/e2e/**/*.ts'
